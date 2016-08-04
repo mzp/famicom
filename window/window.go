6e0f031cbf9fad4b072aad9a98e0df481518aace
mzp <mzpppp@gmail.com>
2016-08-05 08:00:33 +0900

    :hocho: split OpenGL Window utility

---
+++ window/window.go
@@ -0,0 +1,91 @@
+package window
+
+import (
+	"fmt"
+	"image"
+	"image/draw"
+
+	"github.com/go-gl/gl/all-core/gl"
+	"github.com/go-gl/glfw/v3.2/glfw"
+)
+
+func CreateWindow(title string, f func() image.Image) {
+	err := glfw.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer glfw.Terminate()
+
+	window, err := glfw.CreateWindow(640, 480, title, nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	window.MakeContextCurrent()
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	gl.Enable(gl.TEXTURE_2D)
+
+	for !window.ShouldClose() {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		texture, err := newTexture(f())
+
+		if err != nil {
+			panic(err)
+		}
+
+		gl.ActiveTexture(gl.TEXTURE0)
+		gl.BindTexture(gl.TEXTURE_2D, texture)
+
+		gl.Begin(gl.POLYGON)
+
+		gl.TexCoord2f(0, 0)
+		gl.Vertex2f(-1, -1)
+
+		gl.TexCoord2f(0, 1)
+		gl.Vertex2f(-1, 1)
+
+		gl.TexCoord2f(1, 1)
+		gl.Vertex2f(1, 1)
+
+		gl.TexCoord2f(1, 0)
+		gl.Vertex2f(1, -1)
+		gl.End()
+
+		window.SwapBuffers()
+		glfw.PollEvents()
+	}
+}
+
+func newTexture(img image.Image) (uint32, error) {
+	rgba := image.NewRGBA(img.Bounds())
+	if rgba.Stride != rgba.Rect.Size().X*4 {
+		return 0, fmt.Errorf("unsupported stride")
+	}
+	draw.Draw(rgba, rgba.Bounds(), img, image.Point{0, 0}, draw.Src)
+
+	var texture uint32
+	gl.GenTextures(1, &texture)
+	gl.ActiveTexture(gl.TEXTURE0)
+	gl.BindTexture(gl.TEXTURE_2D, texture)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+	gl.TexImage2D(
+		gl.TEXTURE_2D,
+		0,
+		gl.RGBA,
+		int32(rgba.Rect.Size().X),
+		int32(rgba.Rect.Size().Y),
+		0,
+		gl.RGBA,
+		gl.UNSIGNED_BYTE,
+		gl.Ptr(rgba.Pix))
+
+	return texture, nil
+}
