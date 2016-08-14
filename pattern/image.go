34704f8be470df2920cefaca8f90970e2b385b6c
mzp <mzpppp@gmail.com>
2016-08-14 12:21:36 +0900

    :lipstick: wrap patten setter

--- pattern/image.go
+++ pattern/image.go
@@ -5,11 +5,19 @@ import (
 	"image/color"
 )
 
+func set(img *image.RGBA, x, y int, c color.Color) {
+	min, max := img.Rect.Min, img.Rect.Max
+	width := max.X - min.X
+	height := max.Y - min.Y
+
+	img.Set(x%width+min.X, y%height+min.Y, c)
+}
+
 func PutImage(img *image.RGBA, x int, y int, pattern Pattern, pallets []color.Color) {
 	for py, row := range pattern {
 		for px, v := range row {
 			c := pallets[v]
-			img.Set(x+px, y+py, c)
+			set(img, x+px, y+py, c)
 		}
 	}
 }
