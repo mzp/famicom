1439b1ca1792f31841aceb9f5511db303e8928ee
mzp <mzpppp@gmail.com>
2016-08-16 07:44:25 +0900

    :lipstick: use mirror settings at memory.ReadRange

--- memory/memory.go
+++ memory/memory.go
@@ -30,13 +30,19 @@ func (memory *Memory) Read(address uint16) uint8 {
 }
 
 func (memory *Memory) Read16(address uint16) uint16 {
-	low := memory.Data[address]
-	high := memory.Data[address+1]
+	low := memory.Read(address)
+	high := memory.Read(address + 1)
 	return uint16(high)<<8 | uint16(low)
 }
 
 func (memory *Memory) ReadRange(address uint16, size uint16) []byte {
-	return memory.Data[address : address+size]
+	result := make([]byte, size)
+
+	for i := uint16(0); i < size; i++ {
+		result[i] = memory.Read(address + i)
+	}
+
+	return result
 }
 
 func (memory *Memory) Write(address uint16, value byte) {
